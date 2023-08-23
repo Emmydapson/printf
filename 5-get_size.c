6a7632c03b6fed659ae148bc2d21e02d4ef31f0d
emmydapson <iloriemmanuel00@gmail.com>
2023-08-22 20:37:59 -0700

    printf task0-15 all done

---
+++ 5-get_size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format:The Formatted string containing conversion specifiers
+ * @i: current index in the format string
+ *
+ * Return: Size specification for the current conversion specifier
+ */
+int get_size(const char *format, int *i)
+{
+	int current_i = *i + 1;
+	int size = 0;
+
+	if (format[current_i] == 'l')
+		size = S_LONG;
+	else if (format[current_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = current_i - 1;
+	else
+		*i = current_i;
+
+	return (size);
+}
+
---
+++ 6-get_width.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * get_width - Calculates the width for printing
+ * @format:The Formatted string containing conversion specifier
+ * @i: current index in the format string
+ * @list: va_list of arguments to be printed
+ *
+ * Return:Calculated width for the current conversion specifier
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int current_i;
+	int width = 0;
+
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		if (is_digit(format[current_i]))
+		{
+			width *= 10;
+			width += format[current_i] - '0';
+		}
+		else if (format[current_i] == '*')
+		{
+			current_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	*i = current_i - 1;
+
+	return (width);
+}
+
